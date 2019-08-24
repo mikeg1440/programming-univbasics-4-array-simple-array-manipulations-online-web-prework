0d117d5ef4ba9916a2e0af0cbd122d7cd45f3130
Michael Gaudreau <gaudreaum01@gmail.com>
2019-08-24 16:34:39 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_push(array, element)
+  array.push(element)  
+end
+
+def using_unshift(array, element)
+  array.unshtift(element)                     
+end
+
+def using_pop(array)
+  array.pop()
+end
+
+def pop_with_args(array, pop_num)
+  array.pop(pop_num)
+end
+
+def shift_with_args(array, shift_num)
+  array.shift(shift_num)
+end
+
+def using_concat(array_one, array_two)
+  array_one.concat(array_two)
+end
