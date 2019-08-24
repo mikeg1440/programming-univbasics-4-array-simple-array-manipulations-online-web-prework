c89e8d4d77129eb28ca7141e14ef2fd627b132e4
Michael Gaudreau <gaudreaum01@gmail.com>
2019-08-24 16:37:53 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,6 +2,10 @@ def using_push(array, element)
   array.push(element)  
 end
 
+def using_shift(array)
+  
+end
+
 def using_unshift(array, element)
   array.unshift(element)                     
 end
